ff9e446faa981348cee0a54a41296d5357e2440a
somankasekhar1 <153889135+somankasekhar1@users.noreply.github.com>
2025-12-23 12:46:58 +0000

    added two sum java solution

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        TwoSum solver = new TwoSum();
+        int[] result = solver.twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}
